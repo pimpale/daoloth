cfea1f8de0586d2e4d8f1325fa7c55c4660e66eb
gpi <gpimpale29@gmail.com>
2019-07-27 09:22:10 -0700

    stuff

--- src/daoloth/core.clj
+++ src/daoloth/core.clj
@@ -13,23 +13,10 @@
   (let [w (.getWidth c)
         h (.getHeight c)]
     (doto g
-      (draw (polygon [0 h] [(/ w 4) 0] [(/ w 2) (/ h 2)] [w (/ h 2)] [0 h])
-            (style :foreground java.awt.Color/BLACK
-                   :background (color 128 128 128 128)
-                   :stroke     (stroke :width 4)))
       (.setColor (color 224 224 0 128))
-      (.fillRect 0 0 (/ w 2) (/ h 2))
-      (.setColor (color 0 224 224 128))
-      (.fillRect 0 (/ h 2) (/ w 2) (/ h 2))
-      (.setColor (color 224 0 224 128))
-      (.fillRect (/ w 2) 0 (/ w 2) (/ h 2))
-      (.setColor (color 224 0 0 128))
-      (.fillRect (/ w 2) (/ h 2) (/ w 2) (/ h 2))
-      (.setColor (color 0 0 0))
-
       (.drawImage img, 0, 0, nil)
 
-      (.drawString "Hello. This is a canvas example" 20 20))))
+      )))
 
 
 
@@ -37,7 +24,8 @@
   (invoke-later
     (->
       (frame
-        :title "Canvas Example"
+        :title "Daoloth"
+        :on-close :exit
         :width 1920 :height 1080
         :content
         (canvas :id :canvas :background "#BBBBDD" :paint paint))
