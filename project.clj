ed3de17974eca7d2ebb820b4b4cfd5c3e1bf1c79
gpi <gpimpale29@gmail.com>
2019-07-29 09:55:56 -0700

    converted to cljstring

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Unlicense"
             :url "https://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [seesaw "1.5.1-SNAPSHOT"]
-                 [cheshire "5.8.1"]]
+                 [seesaw "1.5.1-SNAPSHOT"]]
   :main daoloth.core
   :target-path "target/%s"
   :plugins [[lein-cljfmt "0.6.4"]]
--- src/daoloth/core.clj
+++ src/daoloth/core.clj
@@ -2,40 +2,57 @@
   (:gen-class)
   (:import [javax.imageio ImageIO]
            [java.awt.image BufferedImage])
-  (:require [clojure.java.io :as io]
-            [cheshire.core :as json])
+  (:require [clojure.java.io :as io])
   (:use seesaw.core
         seesaw.graphics
         seesaw.color))
 
-; Dungeon Struct
-(defrecord Dungeon [name grid])
 
-; Tile Struct
-(defrecord Tile [name level image])
+; serialize to string
+(defn serialize [obj] (pr-str obj))
+
+; deserialize string to object
+(defn deserialize [str] (read-string str))
+
 
 ; returns the image loaded from this resource path
 (defn load-image [resource-path]
   (ImageIO/read (io/resource resource-path)))
 
-; returns a map of the tiles associating char to tile
-(defn load-tile-map [resource-path]
-  ; we insert each tile described inside of the json into the map
+; returns a map of the tiles associating names to tiles
+(defn load-tiles [resource-path]
+  ; we insert each tile described inside into the map
   (reduce
-    (fn [map jsonentry]
+    (fn [map entry]
       (assoc map
-             ; get char that represents in map
-             (get "char" jsonentry)
-             ; create tile
-             (Tile. (get "name" jsonentry)
-                    (get "level" jsonentry)
-                    (load-image (get "img" jsonentry)))))
-      {} ; add them to empty map containing the char:tile binding
-      ; get the list at the root of maptiles/maptiles.json
-      (->
-        (io/resource resource-path)
-        (slurp)
-        (json/decode))))
+             ; get name that represents in map
+             (get "name" entry)
+             ; create tile from values in map
+             (update entry "img" load-image)))
+    {} ; add them to empty map containing the name:tile binding
+    ; get the list of mappings
+    (->
+      (io/resource resource-path)
+      (slurp)
+      (deserialize))))
+
+
+; returns a map associating dungeons with names
+(defn load-dungeons [resource-path]
+  (reduce
+    (fn [map entry]
+      (assoc map
+             ; get name that identifies dungeon
+             (get "name" entry)
+             ; create Dungeon
+             entry))
+    {} ; empty map containing name:dungeon mapping
+    ; get the list of mappings
+    (->
+      (io/resource resource-path)
+      (slurp)
+      (deserialize))))
+
 
 (defn paint [c g]
   (let [w (.getWidth c)
@@ -45,15 +62,18 @@
       ;(.drawImage img, 0, 0, nil)
       )))
 
-(def tile-map (load-tile-map "maptiles/maptiles.json"))
+
+
 
 (defn -main [& args]
-  (invoke-later
-   (->
-    (frame
-     :title "Daoloth"
-     :on-close :exit
-     :width 1920 :height 1080
-     :content (canvas :id :canvas :paint paint))
-    pack!
-    show!)))
+  (let [tile-map (load-tiles "tile.cljstr")
+        dungeons (load-dungeons "dungeon.cljstr")]
+    (invoke-later
+      (->
+        (frame
+          :title "Daoloth"
+          :on-close :exit
+          :width 1920 :height 1080
+          :content (canvas :id :canvas :paint paint))
+        pack!
+        show!))))
